da8535a1783c9e0e5342000873388c1ca35e1aa2
lianlian51 <3282125330@qq.com>
2021-05-26 23:47:11 +0800

    字符串的排列

---
+++ offer38/offer38/test.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+
+void Permutation(char* pStr, char* pBegin);
+
+void Permutation(char* str)
+{
+	if (str == nullptr)
+		return;
+	Permutation(str, str);
+}
+void Permutation(char* pStr, char* pBegin)
+{
+	if (*pBegin == '\0')
+	{
+		printf("%s ", pStr);
+	}
+	else
+	{
+		for (char* pCh = pBegin; *pCh != '\0'; ++pCh)
+		{
+			char temp = *pCh;
+			*pCh = *pBegin;
+			*pBegin = temp;
+
+			Permutation(pStr, pBegin + 1);
+
+			temp = *pCh;
+			*pCh = *pBegin;
+			*pBegin = temp;
+		}
+	}
+}
+
+
+// ====================≤‚ ‘¥˙¬Î====================
+void Test(char* pStr)
+{
+	if (pStr == nullptr)
+		printf("Test for nullptr begins:\n");
+	else
+		printf("Test for %s begins:\n", pStr);
+
+	Permutation(pStr);
+
+	printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+	Test(nullptr);
+
+	char string1[] = "";
+	Test(string1);
+
+	char string2[] = "a";
+	Test(string2);
+
+	char string3[] = "ab";
+	Test(string3);
+
+	char string4[] = "abc";
+	Test(string4);
+
+	return 0;
+}
